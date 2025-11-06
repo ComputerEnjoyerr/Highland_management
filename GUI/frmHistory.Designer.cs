662ade4d3f54b6364037b30df670d06c228b046b
chanhuylonlon <ailopchanhuy@gmail.com>
2025-11-07 02:22:37 +0700

    feat: Load data Invoice History, Bill Info, Find Bill by Employee, Customer History Function

--- GUI/frmHistory.Designer.cs
+++ GUI/frmHistory.Designer.cs
@@ -28,48 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            dgvHistoryBill = new DataGridView();
             panel1 = new Panel();
             groupBox2 = new GroupBox();
-            button2 = new Button();
-            button3 = new Button();
-            button1 = new Button();
-            textBox2 = new TextBox();
+            btnChooseCustomer = new Button();
+            btnClean = new Button();
+            btnChooseEmployee = new Button();
+            txtFindCustomer = new TextBox();
             label2 = new Label();
-            textBox1 = new TextBox();
+            txtFindEmployee = new TextBox();
             label1 = new Label();
             panel2 = new Panel();
+            dgvBill = new DataGridView();
             groupBox1 = new GroupBox();
-            dateTimePicker2 = new DateTimePicker();
+            dtCreateDate = new DateTimePicker();
             label8 = new Label();
-            dateTimePicker1 = new DateTimePicker();
+            dtCreateTime = new DateTimePicker();
             label7 = new Label();
-            textBox6 = new TextBox();
+            txtTable = new TextBox();
             label6 = new Label();
-            textBox5 = new TextBox();
+            txtCustomerName = new TextBox();
             label5 = new Label();
-            textBox4 = new TextBox();
+            txtEmployeeName = new TextBox();
             label4 = new Label();
-            textBox3 = new TextBox();
+            txtBillId = new TextBox();
             label3 = new Label();
-            dataGridView2 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvHistoryBill).BeginInit();
             panel1.SuspendLayout();
             groupBox2.SuspendLayout();
             panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBill).BeginInit();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dgvHistoryBill
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(3, 111);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(591, 596);
-            dataGridView1.TabIndex = 12;
+            dgvHistoryBill.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvHistoryBill.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvHistoryBill.Location = new Point(3, 111);
+            dgvHistoryBill.Name = "dgvHistoryBill";
+            dgvHistoryBill.RowHeadersWidth = 51;
+            dgvHistoryBill.Size = new Size(591, 596);
+            dgvHistoryBill.TabIndex = 12;
+            dgvHistoryBill.CellContentClick += dgvHistoryBill_CellContentClick;
+            dgvHistoryBill.DoubleClick += dgvHistoryBill_DoubleClick;
             // 
             // panel1
             // 
@@ -83,12 +85,12 @@
             // groupBox2
             // 
             groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox2.Controls.Add(button2);
-            groupBox2.Controls.Add(button3);
-            groupBox2.Controls.Add(button1);
-            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(btnChooseCustomer);
+            groupBox2.Controls.Add(btnClean);
+            groupBox2.Controls.Add(btnChooseEmployee);
+            groupBox2.Controls.Add(txtFindCustomer);
             groupBox2.Controls.Add(label2);
-            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(txtFindEmployee);
             groupBox2.Controls.Add(label1);
             groupBox2.Location = new Point(3, 0);
             groupBox2.Name = "groupBox2";
@@ -97,39 +99,42 @@
             groupBox2.TabStop = false;
             groupBox2.Text = "Tìm kiếm";
             // 
-            // button2
+            // btnChooseCustomer
             // 
-            button2.Location = new Point(328, 60);
-            button2.Name = "button2";
-            button2.Size = new Size(89, 39);
-            button2.TabIndex = 13;
-            button2.Text = "Chọn KH";
-            button2.UseVisualStyleBackColor = true;
+            btnChooseCustomer.Location = new Point(328, 60);
+            btnChooseCustomer.Name = "btnChooseCustomer";
+            btnChooseCustomer.Size = new Size(89, 39);
+            btnChooseCustomer.TabIndex = 13;
+            btnChooseCustomer.Text = "Chọn KH";
+            btnChooseCustomer.UseVisualStyleBackColor = true;
+            btnChooseCustomer.Click += button2_Click;
             // 
-            // button3
+            // btnClean
             // 
-            button3.Location = new Point(433, 20);
-            button3.Name = "button3";
-            button3.Size = new Size(89, 39);
-            button3.TabIndex = 14;
-            button3.Text = "Hoàn tác";
-            button3.UseVisualStyleBackColor = true;
+            btnClean.Location = new Point(433, 20);
+            btnClean.Name = "btnClean";
+            btnClean.Size = new Size(89, 39);
+            btnClean.TabIndex = 14;
+            btnClean.Text = "Hoàn tác";
+            btnClean.UseVisualStyleBackColor = true;
+            btnClean.Click += btnClean_Click;
             // 
-            // button1
+            // btnChooseEmployee
             // 
-            button1.Location = new Point(328, 20);
-            button1.Name = "button1";
-            button1.Size = new Size(89, 39);
-            button1.TabIndex = 15;
-            button1.Text = "Chọn NV";
-            button1.UseVisualStyleBackColor = true;
+            btnChooseEmployee.Location = new Point(328, 20);
+            btnChooseEmployee.Name = "btnChooseEmployee";
+            btnChooseEmployee.Size = new Size(89, 39);
+            btnChooseEmployee.TabIndex = 15;
+            btnChooseEmployee.Text = "Chọn NV";
+            btnChooseEmployee.UseVisualStyleBackColor = true;
+            btnChooseEmployee.Click += button1_Click;
             // 
-            // textBox2
+            // txtFindCustomer
             // 
-            textBox2.Location = new Point(113, 60);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(209, 30);
-            textBox2.TabIndex = 11;
+            txtFindCustomer.Location = new Point(113, 60);
+            txtFindCustomer.Name = "txtFindCustomer";
+            txtFindCustomer.Size = new Size(209, 30);
+            txtFindCustomer.TabIndex = 11;
             // 
             // label2
             // 
@@ -140,12 +145,12 @@
             label2.TabIndex = 9;
             label2.Text = "Khách hàng:";
             // 
-            // textBox1
+            // txtFindEmployee
             // 
-            textBox1.Location = new Point(113, 29);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(209, 30);
-            textBox1.TabIndex = 12;
+            txtFindEmployee.Location = new Point(113, 29);
+            txtFindEmployee.Name = "txtFindEmployee";
+            txtFindEmployee.Size = new Size(209, 30);
+            txtFindEmployee.TabIndex = 12;
             // 
             // label1
             // 
@@ -158,27 +163,37 @@
             // 
             // panel2
             // 
+            panel2.Controls.Add(dgvBill);
             panel2.Controls.Add(groupBox1);
-            panel2.Controls.Add(dataGridView2);
             panel2.Dock = DockStyle.Right;
             panel2.Location = new Point(600, 0);
             panel2.Name = "panel2";
             panel2.Size = new Size(392, 710);
             panel2.TabIndex = 10;
             // 
+            // dgvBill
+            // 
+            dgvBill.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvBill.Dock = DockStyle.Fill;
+            dgvBill.Location = new Point(0, 224);
+            dgvBill.Name = "dgvBill";
+            dgvBill.RowHeadersWidth = 51;
+            dgvBill.Size = new Size(392, 486);
+            dgvBill.TabIndex = 2;
+            // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.Controls.Add(dtCreateDate);
             groupBox1.Controls.Add(label8);
-            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(dtCreateTime);
             groupBox1.Controls.Add(label7);
-            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(txtTable);
             groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(txtCustomerName);
             groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(txtEmployeeName);
             groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(txtBillId);
             groupBox1.Controls.Add(label3);
             groupBox1.Dock = DockStyle.Top;
             groupBox1.Location = new Point(0, 0);
@@ -188,13 +203,13 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Chi tiết hóa đơn";
             // 
-            // dateTimePicker2
+            // dtCreateDate
             // 
-            dateTimePicker2.Format = DateTimePickerFormat.Time;
-            dateTimePicker2.Location = new Point(140, 187);
-            dateTimePicker2.Name = "dateTimePicker2";
-            dateTimePicker2.Size = new Size(171, 30);
-            dateTimePicker2.TabIndex = 16;
+            dtCreateDate.Format = DateTimePickerFormat.Time;
+            dtCreateDate.Location = new Point(140, 187);
+            dtCreateDate.Name = "dtCreateDate";
+            dtCreateDate.Size = new Size(171, 30);
+            dtCreateDate.TabIndex = 16;
             // 
             // label8
             // 
@@ -205,13 +220,13 @@
             label8.TabIndex = 6;
             label8.Text = "Ngày tạo:";
             // 
-            // dateTimePicker1
+            // dtCreateTime
             // 
-            dateTimePicker1.Format = DateTimePickerFormat.Time;
-            dateTimePicker1.Location = new Point(140, 156);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(171, 30);
-            dateTimePicker1.TabIndex = 17;
+            dtCreateTime.Format = DateTimePickerFormat.Time;
+            dtCreateTime.Location = new Point(140, 156);
+            dtCreateTime.Name = "dtCreateTime";
+            dtCreateTime.Size = new Size(171, 30);
+            dtCreateTime.TabIndex = 17;
             // 
             // label7
             // 
@@ -222,12 +237,12 @@
             label7.TabIndex = 7;
             label7.Text = "Thời gian:";
             // 
-            // textBox6
+            // txtTable
             // 
-            textBox6.Location = new Point(140, 122);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(225, 30);
-            textBox6.TabIndex = 12;
+            txtTable.Location = new Point(140, 122);
+            txtTable.Name = "txtTable";
+            txtTable.Size = new Size(225, 30);
+            txtTable.TabIndex = 12;
             // 
             // label6
             // 
@@ -238,12 +253,12 @@
             label6.TabIndex = 8;
             label6.Text = "Bàn ăn:";
             // 
-            // textBox5
+            // txtCustomerName
             // 
-            textBox5.Location = new Point(140, 91);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(225, 30);
-            textBox5.TabIndex = 13;
+            txtCustomerName.Location = new Point(140, 91);
+            txtCustomerName.Name = "txtCustomerName";
+            txtCustomerName.Size = new Size(225, 30);
+            txtCustomerName.TabIndex = 13;
             // 
             // label5
             // 
@@ -254,12 +269,12 @@
             label5.TabIndex = 9;
             label5.Text = "Khách hàng:";
             // 
-            // textBox4
+            // txtEmployeeName
             // 
-            textBox4.Location = new Point(140, 60);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(225, 30);
-            textBox4.TabIndex = 14;
+            txtEmployeeName.Location = new Point(140, 60);
+            txtEmployeeName.Name = "txtEmployeeName";
+            txtEmployeeName.Size = new Size(225, 30);
+            txtEmployeeName.TabIndex = 14;
             // 
             // label4
             // 
@@ -270,12 +285,12 @@
             label4.TabIndex = 10;
             label4.Text = "Nhân viên:";
             // 
-            // textBox3
+            // txtBillId
             // 
-            textBox3.Location = new Point(140, 29);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(225, 30);
-            textBox3.TabIndex = 15;
+            txtBillId.Location = new Point(140, 29);
+            txtBillId.Name = "txtBillId";
+            txtBillId.Size = new Size(225, 30);
+            txtBillId.TabIndex = 15;
             // 
             // label3
             // 
@@ -286,63 +301,55 @@
             label3.TabIndex = 11;
             label3.Text = "Mã:";
             // 
-            // dataGridView2
-            // 
-            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Dock = DockStyle.Fill;
-            dataGridView2.Location = new Point(0, 0);
-            dataGridView2.Name = "dataGridView2";
-            dataGridView2.RowHeadersWidth = 51;
-            dataGridView2.Size = new Size(392, 710);
-            dataGridView2.TabIndex = 2;
-            // 
             // frmHistory
             // 
             AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(992, 710);
-            Controls.Add(dataGridView1);
+            Controls.Add(dgvHistoryBill);
             Controls.Add(panel1);
             Controls.Add(panel2);
             Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             Name = "frmHistory";
             Text = "frmHistory";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Load += frmHistory_Load;
+            DoubleClick += frmHistory_DoubleClick;
+            ((System.ComponentModel.ISupportInitialize)dgvHistoryBill).EndInit();
             panel1.ResumeLayout(false);
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
             panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvBill).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dgvHistoryBill;
         private Panel panel1;
         private Panel panel2;
         private GroupBox groupBox1;
-        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dtCreateDate;
         private Label label8;
-        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dtCreateTime;
         private Label label7;
-        private TextBox textBox6;
+        private TextBox txtTable;
         private Label label6;
-        private TextBox textBox5;
+        private TextBox txtCustomerName;
         private Label label5;
-        private TextBox textBox4;
+        private TextBox txtEmployeeName;
         private Label label4;
-        private TextBox textBox3;
+        private TextBox txtBillId;
         private Label label3;
-        private DataGridView dataGridView2;
+        private DataGridView dgvBill;
         private GroupBox groupBox2;
-        private Button button2;
-        private Button button3;
-        private Button button1;
-        private TextBox textBox2;
+        private Button btnChooseCustomer;
+        private Button btnClean;
+        private Button btnChooseEmployee;
+        private TextBox txtFindCustomer;
         private Label label2;
-        private TextBox textBox1;
+        private TextBox txtFindEmployee;
         private Label label1;
     }
 }
