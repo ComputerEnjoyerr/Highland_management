08347ede3ebf27cd3fccf969dc89914c454a4839
Thanh Phu <phutram2005@gmail.com>
2025-11-18 20:22:44 +0700

    wip: Notification's structure

---
+++ BLL/BLL_Notification.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class BLL_Notification
+    {
+        private readonly DAL_Notification _dalNotification = new();
+        public List<Notification> GetAll()
+        {
+            return _dalNotification.GetAll();
+        }
+        public Notification GetById(string id)
+        {
+            return _dalNotification.GetById(id);
+        }
+        public Notification GetByName(string name)
+        {
+            return _dalNotification.GetByName(name);
+        }
+        public List<Notification> GetByEmployeeId(string employeeId)
+        {
+            return _dalNotification.GetByEmployeeId(employeeId);
+        }
+
+        public void Add(Notification notification)
+        {
+            _dalNotification.Add(notification);
+        }
+
+        public void Remove(string id)
+        {
+            _dalNotification.Remove(id);
+        }
+
+        public void UpdateReadStatus(string id, bool isRead)
+        {
+            var notification = _dalNotification.GetById(id);
+            if (notification != null)
+            {
+                notification.IsRead = isRead;
+                // Assuming there's an Update method in DAL_Notification
+                _dalNotification.Update(notification);
+            }
+        }
+    }
+}
---
+++ DAL/DAL_Notification.cs
@@ -0,0 +1,83 @@
+﻿using DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_Notification
+    {
+        private readonly HighlandsContext _context = new();
+
+        public List<Notification> GetAll()
+        {
+            return _context.Notifications
+                .Include(n => n.Branch)
+                .Include(n => n.Employee)
+                .ToList();
+        }
+
+        public Notification GetById(string id)
+        {
+            return _context.Notifications
+                .Include(n => n.Branch)
+                .Include(n => n.Employee)
+                .FirstOrDefault(n => n.Id == id);
+        }
+
+        public Notification GetByName(string name)
+        {
+            return _context.Notifications
+                .Include(n => n.Branch)
+                .Include(n => n.Employee)
+                .FirstOrDefault(n => n.Title == name);
+        }
+
+        public List<Notification> GetByEmployeeId(string employeeId)
+        {
+            return _context.Notifications
+                .Include(n => n.Branch)
+                .Include(n => n.Employee)
+                .Where(n => n.EmployeeId == employeeId)
+                .ToList();
+        }
+
+        public void Add(Notification notification)
+        {
+            _context.Notifications.Add(notification);
+            _context.SaveChanges();
+        }
+
+        public void Remove(string id)
+        {
+            var notification = GetById(id);
+            if (notification != null)
+            {
+                _context.Notifications.Remove(notification);
+                _context.SaveChanges();
+            }
+        }
+
+        public void Update(Notification notification)
+        {
+            var existing = GetById(notification.Id);
+            if (existing != null)
+            {
+                existing.Title = notification.Title;
+                existing.Message = notification.Message;
+                existing.Type = notification.Type;
+                existing.TargetRole = notification.TargetRole;
+                existing.BranchId = notification.BranchId;
+                existing.EmployeeId = notification.EmployeeId;
+                existing.IsRead = notification.IsRead;
+                existing.CreatedAt = notification.CreatedAt;
+                _context.SaveChanges();
+            }
+        }
+
+
+    }
+}
